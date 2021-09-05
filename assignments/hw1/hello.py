5e0894364a05ea2d4fad6213526aafe84c4f04a0
karalici <karalici@g.cofc.edu>
2021-09-05 13:16:10 -0400

    complete hw 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+Name: Iva Karalic
+hello.py
+
+Problem: Write a program that prints "hello,world!" to the screen.
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    print("hello, world!")
+
+main()
